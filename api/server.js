2d5a257869c08e388d97d51a8979afeb0e41794b
Euler <euller_germano1@Hotmail.com>
2023-09-07 04:09:44 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -5,6 +5,29 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 3000
 
+const projeto = {
+    id: 1,
+    descricao: 'Your Way Tracker 3.0'
+  }
+  
+  const proxy = new Proxy(projeto, {
+    get(objetoOriginal, chave, receptor) {
+      console.log(`Alguém pediu a chave ${chave} do projeto`)
+      return Reflect.get(objetoOriginal, chave, receptor)
+    },
+    set(objetoOriginal, chave, valor) {
+      console.log(`Alguém alterou a chave ${chave} do projeto para o valor ${valor}`)
+      objetoOriginal[chave] = valor
+    },
+    
+    const : port = process.env.PORT || 3000,
+  })
+  
+  proxy.descricao = 'Reatividade é mega bacana'
+  
+  console.log(proxy.descricao)
+  
+
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
