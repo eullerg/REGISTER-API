6fc42fb625feced4c4c96a298bdb243f1e020ccf
Euler <euller_germano1@Hotmail.com>
2023-09-07 15:08:32 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -10,35 +10,6 @@ const projeto = {
     descricao: 'Your Way Tracker 3.0'
   }
   
-
-  try {
-switch (req.method) {
-  case "GET":
-    if (id === "OBTER_PROJETO") {
-      return getOBTER_PROJETO();
-    } else {
-      return getOBTER_PROJETO(id as string);
-    }
-  case "POST":
-    if (session?.role === "admin") {
-      return CADASTRAR_PROJETO(req, res);
-    }
-  case "PUT":
-    if (session?.role === "admin") {
-      return ALTERAR_PROJETO(req, res);
-    }
-  case "DELETE":
-    if (session?.role === "admin") {
-      return REMOVER_PROJETO(req, res);
-    }
-  default:
-    return res.status(405).end(`Method ${req.method} Not Allowed`);
-}
-} catch (e) {
-console.log(e);
-}
-
-
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
